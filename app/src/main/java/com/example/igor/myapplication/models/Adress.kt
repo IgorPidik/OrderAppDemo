4d74c64cc1ea2426b0bdff7bcfcc7f649bcdd0c8
Igor <IgorPidik@gmail.com>
2018-06-02 22:14:53 +0200

    Basic customer and adress models added

---
+++ app/src/main/java/com/example/igor/myapplication/models/Adress.kt
@@ -0,0 +1,3 @@
+package com.example.igor.myapplication.models
+
+data class Adress(val country: String, val city: String, val street: String, val houseNumber: Int, val extension: String?)
---
+++ app/src/main/java/com/example/igor/myapplication/models/Customer.kt
@@ -0,0 +1,3 @@
+package com.example.igor.myapplication.models
+
+data class Customer(val firstName: String, val secondName: String, val address: Adress, val phoneNUmber: String)
